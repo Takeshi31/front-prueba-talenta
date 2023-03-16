633be97291fb0975fe30d37841dacff3b7ba3601
takeshi31 <heber87@gmail.com>
2023-03-16 17:35:09 -0500

    feat: Component base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import dataResponse from '../mocks/response.json'
+
+function sendText() {
+  console.log('Click')
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <form>
-        <input placeholder='Lorem ipsum'></input>
-        <button type='submit'>Enviar text</button>
-      </form>
+    <>
+      <header>
+        <form className='form' onSubmit={sendText}>
+          <input placeholder='Lorem ipsum'></input>
+          <button type='submit' >Enviar text</button>
+        </form>
+      </header>
+      <main>
+        {
+          <ul className="items">
+            {
+              dataResponse.map(textData => (
+                <li className="textItem" key={textData.date}>
+                  <div className='textContainer'>
+                    <em>{textData.text}</em>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        }
+      </main>
 
-    </div>
+    </>
   )
 }
 
