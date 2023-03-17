cf0a9734a0afcc8649c44dcd2c5c562415257928
takeshi31 <heber87@gmail.com>
2023-03-17 14:43:03 -0500

    refactor: Separate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,63 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import dataResponse from '../mocks/response.json'
+import { SetStateAction, useEffect, useRef, useState } from 'react'
 
-function sendText() {
-  console.log('Click')
-}
+import ListTextItems from './components/ListTextItems'
+import { useListTextItems } from './hooks/useListTextItems'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const [inputText, setInputText] = useState('')
+  const { listText, sendText } = useListTextItems({inputText})
+  const [error, setError] = useState('')
+  const withoutInput = useRef(true)
+
+  useEffect(() => {
+    if (withoutInput.current) {
+      withoutInput.current = inputText === ''
+      return
+    }
+
+    if(inputText.length < 2) {
+      setError('El texto debe tener al menos dos caracteres');
+      console.log(error)
+
+      return
+    }
+
+    setError('')
+  }, [inputText])
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
+    sendText()
+  }
+
+  const handleChange = (event: { target: { value: SetStateAction<string> } }) => {
+
+    if (event.target.value.toString().startsWith(' ')) {
+      return
+    }
+
+    setInputText(event.target.value)
+  }
 
   return (
     <>
       <header>
-        <form className='form' onSubmit={sendText}>
-          <input placeholder='Lorem ipsum'></input>
+        <form className='form' onSubmit={handleSubmit}>
+          <input value={ inputText } ref={inputRef} onChange={ handleChange }placeholder='Lorem ipsum' />
           <button type='submit' >Enviar text</button>
         </form>
+        {error && <div>{error}</div>}
       </header>
       <main>
         {
-          <ul className="items">
-            {
-              dataResponse.map(textData => (
-                <li className="textItem" key={textData.date}>
-                  <div className='textContainer'>
-                    <em>{textData.text}</em>
-                  </div>
-                </li>
-              ))
-            }
-          </ul>
+          <ListTextItems listItems={listText} />
         }
       </main>
 
     </>
-  )
+  ) 
 }
 
 export default App
