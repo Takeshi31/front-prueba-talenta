73cc0112b0a8a33cbf373811aafe631a9bb403c1
takeshi31 <heber87@gmail.com>
2023-03-24 13:52:37 -0500

    feat: Add delete text

--- src/components/ListTextItems.tsx
+++ src/components/ListTextItems.tsx
@@ -1,12 +1,40 @@
-import { IText } from '../interfaces/text.interface'
+import { useState, useEffect } from 'react';
+import { IText } from '../interfaces/text.interface';
+
+const baseUrl = 'http://localhost:3001/';
+const endpointName = 'delete-text';
 
 export default function ListTextItems(props: { listItems: IText[] }) {
-  const { listItems } = props
+  const { listItems } = props;
+  const [items, setItems] = useState<IText[]>([]);
+
+  useEffect(() => {
+    setItems(listItems);
+  }, [listItems]);
+
+  function deleteText(id: number) {
+    fetch(`${baseUrl}${endpointName}/${id.toString()}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Error code ${response.status}`);
+        }
+      })
+      .then((data) => {
+        setItems(data);
+        console.log(data);
+      })
+      .catch((err) => console.error('ERROR: ', err.message));
+  }
+
   return (
-    listItems && <ul className="items">
-      {
-        listItems.map((textData: IText, index: number) => (
-          <li className="textItem" key={`${index}-${textData.date}`}>
+    items && (
+      <ul className="items">
+        {items.map((textData: IText, index: number) => (
+          <li className="textItem" key={`${index}-${textData.date}`} onClick={() => deleteText(textData.id)}>
             <div className='textContainer'>
               <h4>{textData.date}</h4>
               <p>
@@ -14,8 +42,8 @@ export default function ListTextItems(props: { listItems: IText[] }) {
               </p>
             </div>
           </li>
-        ))
-      }
-    </ul>
-  )
+        ))}
+      </ul>
+    )
+  );
 }
